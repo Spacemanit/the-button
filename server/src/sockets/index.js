f3df0d35c85cba8a21f6f19a9ea9304acaec55e9
Manit Bisht <manit.bisht2007@gmail.com>
2026-06-25 11:11:58 +0530

    bug fix for client

--- server/src/sockets/index.js
+++ server/src/sockets/index.js
@@ -30,7 +30,10 @@ const startCountdown = (io) => {
 const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: process.env.CLIENT_URL || "http://localhost:5173",
+      origin: [
+        process.env.CLIENT_URL,
+        "http://localhost:5173"
+      ],
       methods: ["GET", "POST"]
     }
   });
