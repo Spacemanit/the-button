303c2b22246aa9f60bff442f72a5827771dae5a6
Manit Bisht <manit.bisht2007@gmail.com>
2026-06-30 10:34:52 +0530

    leaderboard order reversed

--- server/src/controllers/leaderboard.controller.js
+++ server/src/controllers/leaderboard.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 
 export const getLeaderboard = async () => {
   return await User.find()
-    .sort({ waitTime: -1 })
+    .sort({ waitTime: 1 })
     .limit(10)
     .select("username waitTime colorTier -_id");
 };
